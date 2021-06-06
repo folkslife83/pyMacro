ba1c051ed9ee4134cd24f536f512778018c50090
folkslife83 <57788761+folkslife83@users.noreply.github.com>
2021-06-06 22:26:21 +0900

    folder_caution

--- hClicker.py
+++ hClicker.py
@@ -1,6 +1,16 @@
 import pyautogui
+import time
 
 #lstIcon = pyautogui.locateOnScreen('images/nListIcon.png')
-lstIcon = pyautogui.locateOnScreen('images/nHH.png')
-pyautogui.moveTo(lstIcon)
 
+h1 = pyautogui.locateOnScreen('images/nH.png')
+h2 = pyautogui.locateOnScreen('images/nHH.png')
+
+if pyautogui.click(h1) :
+    print("heart")
+else:
+    pyautogui.click((90,601))
+    time.sleep(1)
+    pyautogui.typewrite(['pagedown'])
+    pyautogui.typewrite(['pagedown'])
+    pyautogui.typewrite(['pagedown'])
