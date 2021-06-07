9693cbdf69e49c10229552e3023b53a616046bd5
folkslife83 <folkslife@naver.com>
2021-06-07 14:20:17 +0900

    3files

---
+++ NmultiClicker.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import NiconClicker
+import Nheart
+
+def mult(num):
+    
+    ranT = 0
+    pyautogui.click((3,500))
+    time.sleep(0.2)
+    pyautogui.typewrite(['home'])
+
+    for i in range(num):
+        NiconClicker.icon()
+        time.sleep(0.5)
+        Nheart.heart()
+
+        pyautogui.click((3,500))
+        pyautogui.hotkey('ctrl','w')
+
+mult(10)
+
+
+
+
+
