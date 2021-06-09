8c8f7ac2bb108740e6c868a905e28ffce37ed3e2
folkslife83 <57788761+folkslife83@users.noreply.github.com>
2021-06-09 20:09:18 +0900

    ing

--- test.py
+++
@@ -1,5 +0,0 @@
-for i in reversed(range(10)):
-        print(i)
-
-for i in range(10,0,-1):
-        print(i)
--- test3.py
+++
@@ -1,4 +0,0 @@
-import ctypes, platform
-
-Active_W = ctypes.windll.user32.GetDesktopWindow()
-ctypes.windll.user32.SetWindowPos(Active_W,0,0,0,0,0,0x0002|0x0001)
