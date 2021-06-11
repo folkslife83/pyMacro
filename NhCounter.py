b9d2e643da11ee69efb7f392a638329aaec78a20
folkslife83 <folkslife@naver.com>
2021-06-11 17:28:43 +0900

    hcounterN

--- NhCounter.py
+++ NhCounter.py
@@ -2,8 +2,85 @@ import pyautogui
 import time
 import random
 import webbrowser
+from openpyxl import load_workbook
+import webbrowser
+from tkinter import *
+import tkinter.ttk as ttk
+from _tkinter import TclError
+import time, ctypes
 pyautogui.FAILSAFE = False
 
-url = "https://blog.naver.com/folkslife/222369468311"
-webbrowser.open(url)
+def brR():
+    pyautogui.click((10,200))
+    time.sleep(0.2)
+    pyautogui.hotkey('win','up')
+    time.sleep(0.2)
+    pyautogui.hotkey('win','left')
+    time.sleep(1)
+    pyautogui.hotkey('ctrl','-')
+    time.sleep(0.2)
+    pyautogui.hotkey('ctrl','-')
+    time.sleep(0.2)
+    pyautogui.hotkey('ctrl','-')
+    time.sleep(1)
+   
+
+def nCount():
+    url = "https://blog.naver.com/folkslife/222369468311"
+    webbrowser.open(url)
+    time.sleep(1)
+    brR()
+    
+def get_clipboard():
+    root = Tk()
+    root.withdraw()
+    result = None
+    try:
+        result = root.clipboard_get()
+    except TclError as e:
+        print('empty clipboard')
+    return result
+
+def getUrl():
+    pyautogui.click(button='right')
+    time.sleep(0.5)
+    pyautogui.typewrite(['e'])
+    return get_clipboard()
+
+def count():
+    pyautogui.moveTo((240,230))
+    url = ""
+    urlList=[]
+    dup = 0
+    while True:        
+        if url != getUrl():
+            url = get_clipboard()
+            dup = 0
+            time.sleep(0.2)
+            urlList.append(url)
+            time.sleep(0.2)
+            pyautogui.typewrite(['down'])
+            time.sleep(0.5)
+
+        elif dup < 2:
+            pyautogui.typewrite(['down'])            
+            dup += 1
+        
+        else:
+            break
+
+    
+
+    pyautogui.click(button='right')
+    time.sleep(0.5)
+    pyautogui.typewrite(['e'])
+
+
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-from openpyxl import load_workbook
-
- 
-#data_only=Ture로 해줘야 수식이 아닌 값으로 받아온다.
-load_wb = load_workbook("test.xlsm", data_only=True)
-
-load_ws = load_wb['list'] #시트 이름으로 불러오기
- 
-
-#print(load_ws['A1'].value) #셀 주소로 값 출력
-print(load_ws.cell(5,2).value)#셀 좌표로 값 출력
+import requests
+from bs4 import BeautifulSoup
+webpage = requests.get("https://m.blog.naver.com/SympathyHistoryList.naver?blogId=folkslife&logNo=222390678309&categoryId=POST")
+soup = BeautifulSoup(webpage.content, "html.parser")
+print(soup)
